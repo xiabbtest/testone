59b5f7c00a36fc484d2516994a7c8b3e8871cfab
unknown <xiabb@wsd.com>
2015-11-18 11:18:46 +0800

    changes log

--- src/autotest1.java
+++ src/autotest1.java
@@ -3,7 +3,7 @@ import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
-
+#print "xiabbtest"
 public class autotest1{
 
     static class TestBean{
